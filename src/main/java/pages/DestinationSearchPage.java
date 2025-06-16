92917277f56fac9350448357a2a289a85a2e5d22
linh.nguyen39 <linh.nguyen39@ntq-solution.com.vn>
2025-06-16 17:32:24 +0700

    Update: - Add more exceptional cases - Fix for 'more voucher(s) available' scenario - Update elementIsVisible, scrollUntilElementVisible, add scrollUntilEitherElementVisible

--- src/main/java/pages/DestinationSearchPage.java
+++ src/main/java/pages/DestinationSearchPage.java
@@ -12,8 +12,8 @@ public class DestinationSearchPage extends BaseTest {
     public String xpath1 = "//android.widget.TextView[@text=\"%s\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView[@text=\"%s\"]"; //short address - voucher desc
     public String xpath2 = "//android.widget.TextView[translate(@text, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')=\"%s\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView[@text=\"%s\"]"; //short address - voucher desc
     public String lblVoucher = xpath1 + " | " + xpath2;
-    public String xpath3 = "//android.widget.TextView[@text=\"%s\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView[contains(@text,\"more voucher available\")]"; //short address
-    public String xpath4 = "//android.widget.TextView[@text=\"%s\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView[contains(@text,\"more vouchers available\")]"; //short address
+    public String xpath3 = "//android.widget.TextView[translate(@text, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')=\"%s\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView[contains(@text,\"more voucher available\")]"; //short address
+    public String xpath4 = "//android.widget.TextView[translate(@text, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')=\"%s\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView[contains(@text,\"more vouchers available\")]"; //short address
     public String lblhiddenVoucher = xpath3 + " | " + xpath4;
     public String btnBack = "//android.widget.EditText[@text=\"Search\"]/ancestor::android.view.ViewGroup/preceding-sibling::android.view.ViewGroup/android.widget.ImageView";
 
@@ -23,30 +23,33 @@ public class DestinationSearchPage extends BaseTest {
 
     public String getVoucherAddressText(String filePath, String sheetName, String rowName) {
         String voucherDesc = classDecl.excelReader.getVoucherDataList(filePath, sheetName, rowName, "Voucher card details").get(0);
-        if (voucherDesc.contains("TM")){
-            voucherDesc = voucherDesc.replaceAll("TM",  "™");
+        if (voucherDesc.contains("TM")) {
+            voucherDesc = voucherDesc.replaceAll("TM", "™");
         }
-        if (voucherDesc.contains("'")){
+        if (voucherDesc.contains("'")) {
             voucherDesc = voucherDesc.replaceAll("'", "’");
         }
 
         String addressVoucher = "";
         String moreVoucherXpath = "//android.widget.TextView[contains(@text, 'more voucher')]/ancestor::android.view.ViewGroup/preceding-sibling::android.view.ViewGroup/android.widget.TextView[1]";
 
-        try {
-            if (voucherDesc.contains("%")){
-                lblVoucherAddress = lblVoucherAddress.replace("%s", voucherDesc);
-                System.out.println("Element found: " + lblVoucherAddress);
-                addressVoucher = classDecl.commonKeyword.getText(lblVoucherAddress);
-            } else {
-                addressVoucher = classDecl.commonKeyword.getText(lblVoucherAddress, voucherDesc);
-            }
-        } catch (Exception e) {
-            if (classDecl.commonKeyword.elementIsVisible(moreVoucherXpath)){
-                addressVoucher = classDecl.commonKeyword.getText(lblVoucherAddress, moreVoucherXpath);
+        if (voucherDesc.contains("%")) {
+            String lblAddress = lblVoucherAddress.replace("%s", voucherDesc);
+            System.out.println("The address after replacing: " + lblAddress);
+            classDecl.commonKeyword.scrollUntilEitherElementVisible(lblAddress, moreVoucherXpath);
 
-            }else {
-                Assert.fail("Element not found");
+            if (classDecl.commonKeyword.elementIsVisible(lblAddress)) {
+                addressVoucher = classDecl.commonKeyword.getText(lblAddress);
+            } else {
+                addressVoucher = classDecl.commonKeyword.getText(moreVoucherXpath);
+            }
+
+        } else {
+            classDecl.commonKeyword.scrollUntilEitherElementVisible(lblVoucherAddress, moreVoucherXpath, voucherDesc);
+            if (classDecl.commonKeyword.elementIsVisible(lblVoucherAddress, voucherDesc)) {
+                addressVoucher = classDecl.commonKeyword.getText(lblVoucherAddress, voucherDesc);
+            } else {
+                addressVoucher = classDecl.commonKeyword.getText(moreVoucherXpath);
             }
         }
         return addressVoucher;
